9640918986ba02d388b8cc4e9ac5f678fe8e4606
StrShell <wh6610@gmail.com>
2024-05-30 17:43:23 +0900

    test

--- amp-project/pages/hlsplayer.tsx
+++ amp-project/pages/hlsplayer.tsx
@@ -1,9 +1,17 @@
+import axios from 'axios';
 import React, { useEffect, useRef } from 'react';
 import VideoPlayer from "../components/video-player";
 import styles from '@/styles/hlsplayer.module.css';
 
-const Hlsplayer = async () => {
-        const staticData = await fetch('http://192.168.0.25:5000/파묘')
+axios.defaults.baseURL = "http://192.168.0.25:5000";
+axios.defaults.responseType = "json"
+
+
+const Hlsplayer = () => {
+    axios.post('/파묘')
+    .then(function (response) {
+      const StaticData = JSON.parse(response.data);
+    })
     return(
     // HLS 플레이어 컴포넌트
         <div className={styles.container}>
