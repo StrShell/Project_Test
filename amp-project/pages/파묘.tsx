ec046aa394e58e6f4e11eb8b8bf66123e3a4d2ec
StrShell <wh6610@gmail.com>
2024-05-30 17:20:47 +0900

    testpl

--- amp-project/pages/파묘.tsx
+++ amp-project/pages/파묘.tsx
@@ -2,12 +2,13 @@ import React, { useEffect, useRef } from 'react';
 import VideoPlayer from "../components/video-player";
 import styles from '@/styles/hlsplayer.module.css';
 
-const Hlsplayer = () => {
+const Hlsplayer = async () => {
+        const staticData = await fetch('http://192.168.0.25:5000/파묘')
     return(
     // HLS 플레이어 컴포넌트
         <div className={styles.container}>
         {/* 비디오 요소 */}
-            <VideoPlayer src="https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8" />
+            <VideoPlayer src="${staticData[0].vod_url_s3}" />
         </div>
     );
 };
