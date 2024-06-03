8f1260a2c5160c830f4965d84acf3c23c2eb4bf0
StrShell <wh6610@gmail.com>
2024-06-03 17:26:00 +0900

    a

--- amp-project/pages/hlsplayer.tsx
+++ amp-project/pages/hlsplayer.tsx
@@ -19,7 +19,7 @@ const Hlsplayer = () => {
         const fetchData = async () => {
             try {
                 const response = await axios.get('/data');
-                setVideoUrl(response.data[0][0]);
+                setVideoUrl(response.data[0]);
                 console.log(videoUrl);
             } catch (error) {
                 console.error('Error fetching data:', error);
