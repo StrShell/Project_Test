11ede320ffd6eca5a9064e8dd6e32e242a12b833
StrShell <wh6610@gmail.com>
2024-06-03 14:57:10 +0900

    json

--- amp-project/pages/hlsplayer.tsx
+++ amp-project/pages/hlsplayer.tsx
@@ -32,7 +32,7 @@ const Hlsplayer = () => {
 
     return(
         <div className={styles.container}>
-            <VideoPlayer src="${staticData[0].vod_url_s3}" />
+            <VideoPlayer src="${StaticData[0].vod_url_s3}" />
         </div>
     );
 };
