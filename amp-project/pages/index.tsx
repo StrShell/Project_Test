35b450c2e254eb656c34ed703f59ce118045f73f
StrShell <wh6610@gmail.com>
2024-06-03 12:44:12 +0900

    al

--- amp-project/pages/index.tsx
+++ amp-project/pages/index.tsx
@@ -7,7 +7,7 @@ import styles from "@/styles/mainpage.module.css";
 import { useState, useEffect } from 'react';
 
 const inter = Inter({ subsets: ["latin"] });
-axios.defaults.baseURL = "http://43.202.4.22:5000";
+axios.defaults.baseURL = "https://test.mainep.xyz:5000";
 axios.defaults.responseType = "json"
 
 export default function Home() {
