adcbc79488e66c113ddc50df4dece6fe0746f0ea
StrShell <wh6610@gmail.com>
2024-06-03 10:12:00 +0900

    lasted

--- amp-project/pages/hlsplayer.tsx
+++ amp-project/pages/hlsplayer.tsx
@@ -16,22 +16,9 @@ const Hlsplayer = () => {
       const StaticData = JSON.parse(response.data);
     })
     return(
-        <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.container}>
-        <nav className={styles.navbar}>
-        <div className={styles.navbarBrand}>
-            <Link href="/">OTT 서비스</Link>
-        </div>
-        <ul className={styles.navbarNav}>
-        </ul>
-        </nav>
-        <div className={styles.container}>
-        // HLS 플레이어 컴포넌트
-        {/* 비디오 요소 */}
             <VideoPlayer src="${staticData[0].vod_url_s3}" />
         </div>
-        </div>
-        </main>
     );
 };
 
